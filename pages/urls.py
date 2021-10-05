28d3d219f39c9bf90ab2ff6186e3c3a339ce8d45
mbanaso chukwudi <chuksmbanasoj@gmail.com>
2021-10-05 02:01:46 -0600

    Updated file

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from pages import views
-
-app_name = "pages"
-urlpatterns = [
-    path('<int:id>/', views.dynamic_lookup_view, name='page_object'),
-    path('', views.pages_view, name='pages'),
-]
--- sapa/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import sapamode
-# Register your models here.
-
-admin.site.register(sapamode)
