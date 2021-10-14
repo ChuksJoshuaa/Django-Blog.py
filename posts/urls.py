081ddc0c76909e592c4bae00a532af893b94c00d
mbanaso chukwudi <chuksmbanasoj@gmail.com>
2021-10-13 23:47:47 -0600

    Updated Css

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from posts import views
 
 urlpatterns = [
     #path("home/", views.homeview, name='post-home'),
+    path("About/", views.aboutview, name='post-about'),
     path('home/', views.list_view.as_view(), name='post-home'),
     path('user/<str:username>', views.Userlist_view.as_view(), name='user-post'),
     path('<int:id>/detail/', views.detail_view.as_view(), name='post-detail'),
